5c31532a6c4aae396b3da4ba2ef418bc8e82eeb5
aaron.d.arthur <aaron.d.arthur@CLC-LAB209-10.TCSD.tooelesd.org>
2018-09-10 14:11:24 -0600

    Testing Commit

---
+++ 9-10 Examples/src/aaronarthur/ClassExamples/Ascii1.java
@@ -0,0 +1,66 @@
+package aaronarthur.ClassExamples;
+// Aaron Arthur 9/18 Ascii art examples
+
+public class Ascii1 {
+
+	public static void main(String[] args) {
+		System.out.println("HH  HH	IIIIII");
+		System.out.println("HH  HH	  II");
+		System.out.println("HHHHHH	  II");
+		System.out.println("HH  HH	  II");
+		System.out.println("HH  HH	IIIIII");	
+		System.out.println("\n");
+		
+		System.out.println("-------- __@");
+		System.out.println("-----  _`\\< _");
+		System.out.println("----  (*)/   (*)");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("\n");
+		
+		System.out.println(" __");
+		System.out.println("( 0>");
+		System.out.println("///\\");
+		System.out.println("\\V_/_");
+		System.out.println("\n");
+		
+
+		System.out.println("   ___                       _       ______             ");
+		System.out.println("  |_  |                     (_)      |  ___|            ");
+		System.out.println("    | |  __ _ __   __ __ _   _  ___  | |_  _   _  _ __  ");
+		System.out.println("    | | / _` |\\ \\ / // _` | | |/ __| |  _|| | | ||  _ \\ ");
+		System.out.println("/\\__/ /| (_| | \\ V /| (_| | | |\\__ \\ | |  | |_| || | | |");
+		System.out.println("\\____/  \\__ _|  \\_/  \\__ _| |_||___/ \\_|   \\__ _||_| |_|");
+		System.out.println("\n");
+		
+		System.out.println("                    $$$$");
+		System.out.println("                    $$$$");
+		System.out.println("           $$$$$$$$$$$$$$$$$");
+		System.out.println("       $$$$$$$$$$$$$$$$$$$$$$$$$");
+		System.out.println("     $$$$$$$        $$$$   $$$$$$$");
+		System.out.println("   $$$$$$           $$$$       $$$$$$$");
+		System.out.println("  $$$$$$            $$$$         $$$$$$");
+		System.out.println("  $$$$$$            $$$$          $$$$$$");
+		System.out.println("  $$$$$$            $$$$");
+		System.out.println("   $$$$$$           $$$$");
+		System.out.println("    $$$$$$$$        $$$$");
+		System.out.println("      $$$$$$$$$$$$$$$$$$$");
+		System.out.println("          $$$$$$$$$$$$$$$$$$");
+		System.out.println("                 $$$$$$$$$$$$$$$$");
+		System.out.println("                    $$$$    $$$$$$$$$$");
+		System.out.println("                    $$$$          $$$$$$$");
+		System.out.println("                    $$$$            $$$$$$");
+		System.out.println("                    $$$$             $$$$$$");
+		System.out.println("$$$$$$$             $$$$            $$$$$$$");
+		System.out.println(" $$$$$$             $$$$          $$$$$$$");
+		System.out.println("  $$$$$$$           $$$$        $$$$$$$$");
+		System.out.println("   $$$$$$$$         $$$$      $$$$$$$$");
+		System.out.println("      $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+		System.out.println("          $$$$$$$$$$$$$$$$$$$$$$$");
+		System.out.println("                  $$$$$$$$");
+		System.out.println("                    $$$$");
+		System.out.println("                    $$$$");                       
+
+		
+	}
+
+}
